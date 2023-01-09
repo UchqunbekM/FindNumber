560583e76d93c629bfa7b3e1de5f4f6caf2380be
UchqunbekM <akin-151@mail.ru>
2023-01-10 01:40:47 +0500

    first commit

---
+++ Lesson1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, high, low, health, userinput;
+            health=5;
+          Random rand=new Random();
+            number=rand.Next(0, 101);
+            low=rand.Next(number-10, number);
+            high=rand.Next(number+1, number+10);
+            Console.WriteLine($"I Think number! Find it. the number higher than {low} and lower than {high}");
+            Console.WriteLine($"You have {health} chance");
+            while (health-->0)
+            {
+                Console.WriteLine("Your number : ");
+                userinput=Convert.ToInt32(Console.ReadLine());
+                if (userinput==number)
+                {
+                    Console.WriteLine("Correct !!!");
+                    break;
+                } else
+                {
+                    Console.WriteLine("Wrong! try again");
+                     
+                }
+                if (health==0)
+                {
+                    Console.WriteLine($"The correct number is {number}");
+
+                }
+
+            }
+
+     
+        }
+    }
+}
